71c3716e9c2c85283bf6517be7a508e6c9db8b89
optilude <optilude@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-06-05 21:38:41 +0000

    It's not zip safe

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ The use case
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
           'setuptools',
