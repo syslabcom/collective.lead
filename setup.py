12717230d3c133c2acccc4b7877a329c0ee767d0
optilude <optilude@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-09-09 00:17:03 +0000

    Tagging 1.0rc2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ The use case
       install_requires=[
           # -*- Extra requirements: -*-
           'setuptools',
-          'SQLAlchemy>=0.3.10,<0.4',
+          'SQLAlchemy>=0.3.10,<0.4dev',
       ],
       entry_points="""
       # -*- Entry points: -*-
